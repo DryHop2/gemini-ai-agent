6daca6e99b3ca4624a8cba489488b2ea5696d65c
DryHop2 <tyler.savon.smith@gmail.com>
2025-06-04 01:26:52 -0600

    add CLI prompt input from user logic

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", 
-                               contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    query = sys.argv[1:]
+    if not query:
+        print("Usage: python main.py 'your prompt here'")
+        print("Example: pythong main.py 'How do I build a calculator app?'")
+        sys.exit(1)
+    prompt = " ".join(query)
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
